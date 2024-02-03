8cf30056b1c3a81e2f21bb001f87ab99ed7e6f26
Bhavya <bhogathibhavya99@gmail.com>
2024-02-03 22:56:47 +0530

    styled for responsiveness

--- weather_app-main/src/pages/Home.jsx
+++ weather_app-main/src/pages/Home.jsx
@@ -12,178 +12,284 @@ import { MobileView } from "../responsive.js";
 // Styled Components
 const Container = styled.div`
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   justify-content: space-evenly;
-  gap:; 10px;
+  gap: 10px;
   height: 85vh;
   margin: 5vh auto;
   width: 56vw;
-  ${MobileView({width:"85vw",margin:"none"})};
-`
-const Search=styled.div`
+
+  @media screen and (max-width: 768px) {
+    width: 85vw;
+    margin: none;
+  }
+`;
+
+const Search = styled.div`
   border: none;
-  background-color:  rgba(128, 126, 227, 0.44);
+  background-color: rgba(128, 126, 227, 0.44);
   border-radius: 111px;
   display: flex;
   justify-content: space-between;
-  padding:5px 20px;
-  height: 40px; 
-  width: 96%;  
-  ${MobileView({width: "88%",height:"30px",marginTop:"80px"})}
-`
-const Input=styled.input`
+  padding: 5px 20px;
+  height: 40px;
+  width: 96%;
+
+  @media screen and (max-width: 768px) {
+    width: 88%;
+    height: 30px;
+    margin-top: 40%;
+  }
+   @media screen and (max-width: 550px) {
+    width: 88%;
+    height: 30px;
+    margin-top:55%;
+  }
+`;
+
+const Input = styled.input`
   border: none;
   background-color: transparent;
   color: rgba(255, 255, 255, 0.78);
   font-size: 30px;
   font-weight: 500;
-  &:hover{
-    outline:none;
-  },
-  &::placeholder {
-    color:whitesmoke;
-    font-size: 18px;
-    padding-top: -50px;   
-    ${MobileView({fontSize:"15px"})}
-  },
-  &:focus{
-    outline:none;
+
+  &:hover {
+    outline: none;
   }
+
+  &::placeholder {
+    color: whitesmoke;
+    font-size: 18px;
+    padding-top: -50px;
+
+    @media screen and (max-width: 768px) {
+      font-size: 15px;
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
   width: 100%;
-  ${MobileView({fontSize:"20px"})}
-`
-const Icon=styled.div`
+
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+const Icon = styled.div`
   color: rgba(255, 255, 255, 0.78);
   cursor: pointer;
   display: flex;
-  align-items:Center;
+  align-items: center;
   justify-content: center;
   height: inherit;
   width: 40px;
-`
-const CurrentWeather=styled.div`
-  color:#fff;
+`;
+
+const CurrentWeather = styled.div`
+  color: #fff;
   display: flex;
   justify-content: space-between;
   height: 38vh;
-  margin-top: 20px;
+  margin-top: 10vh;
   width: 100%;
-  ${MobileView({flexDirection:"column",width:"100%",gap:"10px"})}
-`
-const ReportBox1=styled.div`
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    gap: 2px;
+  }
+`;
+
+// Styled Components
+const ReportBox1 = styled.div`
   border-radius: 6px;
-  background-color: #1746A9;
-  cursor:pointer;
-  height:inherit;
-  padding: 20px;
+  background-color: #1746a9;
+  cursor: pointer;
+  padding: 2%;
   width: 50%;
   transition: all 0.5s ease;
-  &:hover{
-   transform: scale(1.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  &:hover {
+    transform: scale(1.1);
   }
-  ${MobileView({width:"88%"})}
-`
-const City=styled.h3`
-  font-size: 25px;
+
+  @media screen and (max-width: 768px) {
+    width: 88%;
+    height: 100vh;
+  }
+`;
+
+const City = styled.h3`
   font-weight: 500;
-  margin-top: -2px;
-  ${MobileView({marginLeft:"45px"})}
-`
-const WeatherDetails=styled.div`
+  margin-top: 10px;
+
+  @media screen and (max-width: 768px) {
+    margin-left: 0;
+  }
+`;
+
+const WeatherDetails = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 200px;
+  height: auto;
   width: 100%;
-  ${MobileView({flexDirection:"column",alignItems:"center"})}
-`
-const WeatherIcon=styled.div`  
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const WeatherIcon = styled.div`
   display: flex;
-  font-size: 100px;
-  align-items:center;
-  justify-content:center;
+  font-size: 120%;
+  align-items: center;
+  justify-content: center;
   height: inherit;
-  width: 35%;  
-`
-const WeatherReport=styled.div`
+  width: 35%;
+  margin-top: 10%;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    justify-content: center;
+  }
+`;
+
+const WeatherReport = styled.div`
   display: flex;
-  flex-direction:column;
-  padding-left: 10px;
+  flex-direction: column;
+  padding-left: 10%;
   height: inherit;
   width: 55%;
-`
-const Temp=styled.h2`
-  font-size: 100px;
+  text-align: left;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 0;
+    align-items: center;
+  }
+`;
+
+const Temp = styled.h1`
+  font-size: 4em;
   font-weight: 500;
-  margin-top: 0px;
-  ${MobileView({fontSize:"60px",marginLeft:"20px"})}
-`
-const Condition=styled.span`
-  margin-top: -80px;
-  margin-left: 7px;
-  ${MobileView({marginTop:"-60px",marginLeft:"23px"})}
-`
-const ReportBox2=styled.div`
+  margin-top: 20%;
+
+  @media screen and (max-width: 768px) {
+    margin-top:2%;
+  }
+`;
+
+const Condition = styled.span`
+  margin-top: -1em;
+  margin-left: 0.5em;
+
+  @media screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+const ReportBox2 = styled.div`
   border-radius: 6px;
-  cursor:pointer;
-  background-color: #1746A9;
-  display:flex;
-  flex-direction:column;
+  cursor: pointer;
+  background-color: #1746a9;
+  display: flex;
+  flex-direction: column;
   justify-content: space-evenly;
-  height:inherit;
+  height: inherit;
   padding: 20px;
   width: 30%;
   transition: all 0.5s ease;
-  &:hover{
-    transform: scale(1.1)
+
+  &:hover {
+    transform: scale(1.1);
   }
-  ${MobileView({width:"88%",height:"200px"})}
-`
-const Day=styled.div`
+
+  @media screen and (max-width: 768px) {
+    width: 88%;
+    height: 60%;
+    margin-top:5%;
+    display:flex;
+    flex-direction:column;
+  }
+`;
+
+const Day = styled.div`
   display: flex;
   gap: 10px;
   margin-left: 30px;
-  ${MobileView({marginTop:"20px"})}
-`
-const DailyReportContainer=styled.div`
+
+  @media screen and (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const DailyReportContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   height: 160px;
   width: 100%;
-  ${MobileView({marginTop:"185px"})}
-`
-const Image=styled.img`
-  height: 100px;
-  object-fit:contain;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 50%;
+  }
+  @media screen and (max-width: 550px) {
+    margin-top: 80%;
+  }
+`;
+
+const Image = styled.img`
+  height: 70%;
+  object-fit: contain;
   width: 100px;
-`
-const WeatherIconContainer=styled.div`
-  background-color:  #ffffff54;
+`;
+
+const WeatherIconContainer = styled.div`
+  background-color: #ffffff54;
   border-radius: 50%;
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: center;
-  height: 135px;
-  margin-top: -32px;
+  height: 75%;
+  width:75%;
+  margin-top:-20%;
   width: 135px;
-  ${MobileView({height:"110px",width:"110px",marginTop:"5px"})}
-`
-const Message=styled.h3`
+
+  @media screen and (max-width: 768px) {
+    margin-top: 2%;
+  }
+`;
+
+const Message = styled.h3`
   font-weight: 400;
-`
-const DefaultMessage=styled.div`
+`;
+
+const DefaultMessage = styled.div`
   display: flex;
   flex-direction: column;
-  align-items:center;
+  align-items: center;
   justify-content: center;
   width: 100%;
-`
-const WeatherVec=styled.img`
+`;
+
+const WeatherVec = styled.img`
   border-radius: 50%;
-  object-fit: obtain;
+  object-fit: contain;
   height: 180px;
   width: 180px;
-`
+`;
+
+
+
 function Home() {
 
   // STATES
@@ -204,7 +310,7 @@ function Home() {
   // API_KEY
   // Paste your key after "||" in case env not work it will take it
   //Keep In Mind - remove your api key from here before make your app public,use env.
-  const API_KEY=process.env.API_KEY||"Paste Your OpenWeather API KEY HERE"; 
+  const API_KEY=process.env.API_KEY||"aba312ea73142cab0213a2fb67c8f94a"; 
 
   // Current Date
   const today=new Date()
@@ -290,6 +396,23 @@ function Home() {
     }
   }
 
+   const dayz= ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const todayy = new Date();
+          const todayDay = todayy.getDay(); // Get today's day index (0 for Sunday, 1 for Monday, etc.)
+          const todayyDate = todayy.getDate(); // Get today's date
+          // Function to get the day and date for the next 5 days
+          const getNextDays = () => {
+            const nextDays = [];
+            for (let i = 1; i <= 5; i++) {
+              const nextDay = (todayDay + i) % 7; // Calculate next day's index, taking into account the circular nature of the days array
+              const nextDate = todayyDate + i; // Increment the date
+              nextDays.push({ day: dayz[nextDay], date: nextDate });
+            }
+            return nextDays;
+          };
+
+          const nextDays = getNextDays();
+
   return (
     <Container> 
 
@@ -310,24 +433,28 @@ function Home() {
         <CurrentWeather>
 
           <ReportBox1>
-            {currentCity?<City>{`${currentCity} , ${currentCountry}`}</City>:null}            
-            {currentCity?<WeatherDetails>
-              <WeatherIcon>
-                <WeatherIconContainer>
-                   <Image src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="Weather Icon"/>
-                </WeatherIconContainer>
-              </WeatherIcon>
-              <WeatherReport>
-                {currentTemp?<Temp>{`${currentTemp}`}&deg;C</Temp>:null}                
-                <Condition>{weatherCondition}</Condition>
-              </WeatherReport>
-            </WeatherDetails>:
-            <DefaultMessage>
-              <Message>Enter city name to know the weather</Message>
-              <WeatherVec src={weatherVector}/>
-            </DefaultMessage>
-            }            
-          </ReportBox1>
+  {currentCity ? (
+    <>
+      <City>{`${currentCity}, ${currentCountry}`}</City>
+      <WeatherDetails>
+        <WeatherIcon>
+          <WeatherIconContainer>
+            <Image src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="Weather Icon" />
+          </WeatherIconContainer>
+        </WeatherIcon>
+        <WeatherReport>
+          {currentTemp ? <Temp>{`${currentTemp}`}&deg;C</Temp> : null}
+          <Condition>{weatherCondition}</Condition>
+        </WeatherReport>
+      </WeatherDetails>
+    </>
+  ) : (
+    <DefaultMessage>
+      <Message>Enter city name to know the weather</Message>
+      <WeatherVec src={weatherVector} />
+    </DefaultMessage>
+  )}
+</ReportBox1>
 
           <ReportBox2>
             <Day><CalendarTodayOutlinedIcon/>{todayDate}</Day>
@@ -338,14 +465,13 @@ function Home() {
         </CurrentWeather>
 
         {/* DAILY WEATHER SECTION */}
-        <DailyReportContainer>
-          <DailyReport day={days[today.getDay()]} date={today.getDate()} min={minTemp} max={maxTemp} icon={weatherIcon}/>      
-          <DailyReport day="Tue" date="3"/>  
-          <DailyReport day="Wed" date="4"/>      
-          <DailyReport day="Thu" date="21"/>
-          <DailyReport day="Fri" date="24"/>      
-          <DailyReport day="Sat" date="30"/> 
-        </DailyReportContainer>
+      <DailyReportContainer>
+        <DailyReport day={dayz[todayDay]} date={today.getDate()} min={minTemp} max={maxTemp} icon={weatherIcon}/> {/* Today */}
+        {nextDays.map((dayInfo, index) => (
+          <DailyReport key={index} day={dayInfo.day} date={dayInfo.date} />
+        ))}
+      </DailyReportContainer>
+
           
     </Container>
   );
